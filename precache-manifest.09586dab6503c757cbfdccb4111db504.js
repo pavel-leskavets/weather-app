188604e4c5a3e633b17b15f6f1b518383cb97675
pavel <leskovecpavel7@gmail.com>
2019-07-04 10:32:17 +0300

    Updates

---
+++ precache-manifest.09586dab6503c757cbfdccb4111db504.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e8ff3abe0c4bcd33982fd726ff0b242",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "c98f5024bc331c9d41ea",
+    "url": "/weather-app/static/css/main.fb947e89.chunk.css"
+  },
+  {
+    "revision": "950bfcc3792b7ac9b939",
+    "url": "/weather-app/static/js/2.c407f00e.chunk.js"
+  },
+  {
+    "revision": "c98f5024bc331c9d41ea",
+    "url": "/weather-app/static/js/main.c4c36921.chunk.js"
+  },
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  }
+]);
